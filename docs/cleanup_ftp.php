2b4b51dcb9f160e6d73ae7f75196018fb24391d7
demel42 <demel42@web.de>
2019-06-22 12:15:13 +0000

    Apply fixes from StyleCI

--- docs/cleanup_ftp.php
+++ docs/cleanup_ftp.php
@@ -1,12 +1,12 @@
-<?
+<?php
 
 $guids = [
-		'{06D589CF-7789-44B1-A0EC-6F51428352E6}'
-	];
+        '{06D589CF-7789-44B1-A0EC-6F51428352E6}'
+    ];
 
 foreach ($guids as $guid) {
-	$instIDs = IPS_GetInstanceListByModuleID($guid);
-	foreach ($instIDs as $instID) {
-		NetatmoSecurityOutdoor_CleanupVideoPath($instID);
-	}
+    $instIDs = IPS_GetInstanceListByModuleID($guid);
+    foreach ($instIDs as $instID) {
+        NetatmoSecurityOutdoor_CleanupVideoPath($instID);
+    }
 }

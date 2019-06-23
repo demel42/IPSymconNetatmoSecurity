74a91c8a0800510e1352280bd124f1e69691a58d
IP-Symcon <root@ips-prod>
2019-06-23 10:39:05 +0200

    fix

--- docs/cleanup_ftp.php
+++ docs/cleanup_ftp.php
@@ -1,12 +1,6 @@
 <?php
 
-$guids = [
-        '{06D589CF-7789-44B1-A0EC-6F51428352E6}'
-    ];
-
-foreach ($guids as $guid) {
-    $instIDs = IPS_GetInstanceListByModuleID($guid);
-    foreach ($instIDs as $instID) {
-        NetatmoSecurityOutdoor_CleanupVideoPath($instID);
-    }
+$instIDs = IPS_GetInstanceListByModuleID('{06D589CF-7789-44B1-A0EC-6F51428352E6}');
+foreach ($instIDs as $instID) {
+	NetatmoSecurityOutdoor_CleanupVideoPath($instID, false);
 }

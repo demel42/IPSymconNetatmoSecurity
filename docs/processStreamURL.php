a2d1920ef933abfe732a334dae5384770bd7c513
IP-Symcon <root@ips-prod>
2019-07-16 15:25:46 +0200

    kleine Anpassungen

--- docs/processStreamURL.php
+++ docs/processStreamURL.php
@@ -1,14 +1,19 @@
 <?php
 
-//Author:    Christopher Wansing
-//Created:   13.07.19
-//Modified:  15.07.19
-//Hinweise:
-// - Code for ~HTMLBox:  <iframe width="100%" height="360" src="http://192.168.8.251:3777/hook/Netatmo_Flur/video?live&result=custom"></iframe>
-// - Player Height:      Add &height=xxx to the URL in the iframe. Also change the iframe height accordingly (Player height + 20)
-// - AutoPlay:           Add &autoplay to the URL in the iframe
+// Author:    Christopher Wansing
+// Created:   13.07.19
+// Modified:  15.07.19
+//
+// Hinweise:
+// - Code for ~HTMLBox:  <iframe width="100%" height="360" src="<IPS-URL>/hook/<HookName>/video?live&result=custom"></iframe>
+//                       <IPS-URL> kann sein: "https://<ipmagic-Adresse>" oder "http://<IPS-IP>:3777"
+//                       <HookName> ist der Name der Hook aus der Konfiguration der Kamera
+//                       Anstelle von "video/live" kann das Kommando auch "snapshot/live" etc lauten (siehe README.md)
+//
+// - Player Height:      Add '&height=xxx' to the URL in the iframe. Also change the iframe height accordingly (Player height + 20)
+// - AutoPlay:           Add '&autoplay' to the URL in the iframe
 
-//URL-Parameter parsen
+// URL-Parameter parsen
 $SERVER = json_decode($_IPS['_SERVER'], true);
 $GET = json_decode($_IPS['_GET'], true);
 $height = isset($GET['height']) ? $GET['height'] : '340';
@@ -16,7 +21,7 @@ $autoplay = isset($GET['autoplay']) ? 'autoplay' : '';
 
 $video_id = 'NetatmoStream_' . substr(uniqid(), -4);
 $url = $_IPS['url'];
-$posterURL = @$_IPS['alternate_url']; //preg_replace('/files\/high\/index\.m3u8/', 'snapshot_720.jpg', $url);
+$posterURL = @$_IPS['alternate_url'];
 
 if (preg_match('/\.m3u8$/', $url)) {
     $html = '<link href="https://vjs.zencdn.net/7.6.0/video-js.css" rel="stylesheet">  ';
@@ -31,14 +36,16 @@ if (preg_match('/\.m3u8$/', $url)) {
     $html .= '</script>';
 } elseif (preg_match('/\.jpg$/', $url)) {
     $html = '<img src="' . $url . '" height="' . $height . '">';
-} elseif (preg_match('/\.mp4$/', $url) && (preg_match('/firefox/i', $_SERVER['HTTP_USER_AGENT']) || preg_match('/[5-9]\.[2-9]/', IPS_GetKernelVersion()))) {
-    $html = '<video height="' . $height . '">  ';
-    $html .= '<source src="' . $url . '" type="video/mp4">  ';
-    $html .= '</video>';
 } elseif (preg_match('/\.mp4$/', $url)) {
-    $html = 'MP4 kann nur mit Firefox abgespielt werden, solange noch nicht mindestens IPS 5.2 installiert ist.';
+	if (preg_match('/firefox/i', $_SERVER['HTTP_USER_AGENT']) || preg_match('/[5-9]\.[2-9]/', IPS_GetKernelVersion())) {
+		$html = '<video height="' . $height . '">  ';
+		$html .= '<source src="' . $url . '" type="video/mp4">  ';
+		$html .= '</video>';
+	} else {
+		$html = 'MP4 kann nur mit Firefox abgespielt werden, solange noch nicht mindestens IPS 5.2 installiert ist.';
+	}
 }
 
 $html = preg_replace('/[ ]{2}/', "\n", $html);   //Ersetzt doppelte Leerzeichen durch Zeilenumbrüche
 
-echo $html;     //Ausgabe zurück an das aufrufende Modul
+echo $html;     // Ausgabe zurück an das aufrufende Modul

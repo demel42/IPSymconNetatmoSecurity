7a9771b341a9f67c6e5f54f884f45bbdbf55fe55
IP-Symcon <root@ips-prod>
2019-07-19 12:43:52 +0200

    wird nun intern gehandhabt

--- docs/cleanup_ftp.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$instIDs = IPS_GetInstanceListByModuleID('{06D589CF-7789-44B1-A0EC-6F51428352E6}');
-foreach ($instIDs as $instID) {
-    NetatmoSecurity_CleanupVideoPath($instID, false);
-}

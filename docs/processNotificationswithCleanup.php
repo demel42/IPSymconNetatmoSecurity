d0c9df881dc53a77eb83b3d3461fa1fde7f093b0
IP-Symcon <root@ips-prod>
2019-07-20 17:00:38 +0200

    1. + DeleteEvent() 2. Nutzung des 'refresh_token 3. + getAppToken() - Zugriff auf FUnktionen der Netatmo-App

---
+++ docs/processNotificationswithCleanup.php
@@ -0,0 +1,26 @@
+<?
+
+/* 
+ * Problem: bei Netatmo kann man nur für alle Kameras einstellen, wann benachrichtigt/aufgenommen werden soll.
+ * Wenn man auf der Straßenseite ggfs. Fahrzeuge aufnehmen möchte, würde aber z.B. im Garten ein Rasenmährobotor
+ * als "Fahrzeug" erkannt wird.
+ *
+ * Mit dieser Beispiel-Logik kann man erzeugte Aufnahme direkt wieder löschen
+ */
+
+$scriptName = IPS_GetName($_IPS['SELF']) . '(' . $_IPS['SELF'] . ')';
+
+$instID = $_IPS['InstanceID'];
+if ($instID == 24143 /* Garten */) {
+	$notifications = json_decode($_IPS['new_notifications'], true);
+	foreach ($notifications as $notification) {
+		if (isset($notification['event_type'])) {
+			$event_type = $notification['event_type'];
+			$event_id = $notififaction['id'];	
+			if ($event_type == 'vehicle') {
+				$r = NetatmoSecurity_DeleteEvent($instID, $event_id);
+				IPS_LogMessage($scriptName, 'delete event => ' . $event_id . ($r ? 'ok' : 'fail'));
+			}
+		}
+	}
+}

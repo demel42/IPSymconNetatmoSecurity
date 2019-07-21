63a3b5a15a58e4249a11cfac61d56e8631bfb65f
IP-Symcon <root@ips-prod>
2019-07-21 14:54:28 +0200

    verbesserung

--- docs/processUrlChanged.php
+++ docs/processUrlChanged.php
@@ -7,6 +7,13 @@ $vpn_url = NetatmoSecurity_GetVpnUrl($instID);
 
 IPS_LogMessage($scriptName, 'vpn_url=' . $vpn_url);
 
-/*
- * hier den Code um eventuell notwendige Änderungen nachzuführen
- */
+// hier den Code um eventuell notwendige Änderungen nachzuführen, z.B.
+
+// ID's der HTML-Box-Variablen
+ $varIDs = [
+		1234,
+	];
+
+foreach ($varIDs as $varID) {
+	SetValue($varID, GetValue($varId));
+}

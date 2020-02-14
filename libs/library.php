e7216d39b0192bc2c149e3bd9ed56430d8748e73
IP-Symcon <root@ips-prod>
2020-02-14 11:39:38 +0100

    do_HttpRequest() wieder zurück in common.php

--- libs/library.php
+++ libs/library.php
@@ -119,22 +119,26 @@ trait NetatmoSecurityLibrary
     private function determineVpnUrl()
     {
         $vpn_url = $this->GetBuffer('vpn_url');
+		$this->SendDebug(__FUNCTION__, 'vpn_url=' . $vpn_url, 0);
         return $vpn_url;
     }
 
     private function determineLocalUrl()
     {
         $is_local = $this->GetBuffer('is_local');
+		$this->SendDebug(__FUNCTION__, 'is_local=' . $is_local, 0);
         if (!$is_local) {
             return false;
         }
 
         $local_url = $this->GetBuffer('local_url');
+		$this->SendDebug(__FUNCTION__, 'local_url=' . $local_url, 0);
         if ($local_url != '') {
             return $local_url;
         }
 
         $vpn_url = $this->GetBuffer('vpn_url');
+		$this->SendDebug(__FUNCTION__, 'vpn_url=' . $vpn_url, 0);
         if ($vpn_url == '') {
             return false;
         }
@@ -180,5 +184,100 @@ trait NetatmoSecurityLibrary
             $url = $this->determineVpnUrl();
         }
         return $url;
+    }
+
+    private function do_HttpRequest($url, $header, $postdata, $mode, &$data, &$err)
+    {
+        $this->SendDebug(__FUNCTION__, 'http-' . $mode . ': url=' . $url, 0);
+        $time_start = microtime(true);
+
+        if ($header != '') {
+            $this->SendDebug(__FUNCTION__, '    header=' . print_r($header, true), 0);
+        }
+        if ($postdata != '') {
+            $this->SendDebug(__FUNCTION__, '    postdata=' . print_r($postdata, true), 0);
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        if ($header) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        }
+        switch ($mode) {
+            case 'GET':
+                break;
+            case 'POST':
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+                break;
+            case 'PUT':
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $mode);
+                break;
+            case 'DELETE':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $mode);
+                break;
+        }
+
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        $cdata = curl_exec($ch);
+        $cerrno = curl_errno($ch);
+        $cerror = $cerrno ? curl_error($ch) : '';
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        $duration = round(microtime(true) - $time_start, 2);
+        $this->SendDebug(__FUNCTION__, ' => errno=' . $cerrno . ', httpcode=' . $httpcode . ', duration=' . $duration . 's', 0);
+        $this->SendDebug(__FUNCTION__, '    cdata=' . $cdata, 0);
+
+        $statuscode = 0;
+        $err = '';
+        $data = '';
+
+        if ($cerrno) {
+            $statuscode = IS_SERVERERROR;
+            $err = 'got curl-errno ' . $cerrno . ' (' . $cerror . ')';
+        } elseif ($httpcode != 200) {
+            if ($httpcode == 401) {
+                $statuscode = IS_UNAUTHORIZED;
+                $err = 'got http-code ' . $httpcode . ' (unauthorized)';
+            } elseif ($httpcode == 403) {
+                $statuscode = IS_FORBIDDEN;
+                $err = 'got http-code ' . $httpcode . ' (forbidden)';
+            } elseif ($httpcode == 406) {
+                if (preg_match('#^https://api.netatmo.net/api/dropwebhook#', $url)) {
+                    $data = $cdata;
+                } else {
+                    $statuscode = IS_HTTPERROR;
+                    $err = 'got http-code ' . $httpcode . ' (not acceptable)';
+                }
+            } elseif ($httpcode == 409) {
+                $data = $cdata;
+            } elseif ($httpcode >= 500 && $httpcode <= 599) {
+                $statuscode = IS_SERVERERROR;
+                $err = 'got http-code ' . $httpcode . ' (server error)';
+            } else {
+                $statuscode = IS_HTTPERROR;
+                $err = 'got http-code ' . $httpcode;
+            }
+        } elseif ($cdata == '') {
+            $statuscode = IS_NODATA;
+            $err = 'no data';
+        } else {
+            $jdata = json_decode($cdata, true);
+            if ($jdata == '') {
+                $statuscode = IS_INVALIDDATA;
+                $err = 'malformed response';
+            } else {
+                $data = $cdata;
+            }
+        }
+
+        $this->SendDebug(__FUNCTION__, '    statuscode=' . $statuscode . ', err=' . $err, 0);
+        $this->SendDebug(__FUNCTION__, '    data=' . $data, 0);
+        return $statuscode;
     }
 }

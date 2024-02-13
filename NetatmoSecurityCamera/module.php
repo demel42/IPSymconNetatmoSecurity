44809e08ca730068009af625af25f46bc7bf657b
Ch. Damsky <demel42@web.de>
2024-02-13 15:42:14 +0100

    v1.42.1

--- NetatmoSecurityCamera/module.php
+++ NetatmoSecurityCamera/module.php
@@ -4219,6 +4219,7 @@ class NetatmoSecurityCamera extends IPSModule
 
         $cache_ids = [];
 
+		// Rücksprache mit NT per Mail am 13.02.2024
         $instIDs = IPS_GetInstanceListByModuleID('{06D589CF-7789-44B1-A0EC-6F51428352E6}'); // NetatmoSecurityCamera
         foreach ($instIDs as $instID) {
             @$mediaID = IPS_GetObjectIDByIdent('Events', $instID);
